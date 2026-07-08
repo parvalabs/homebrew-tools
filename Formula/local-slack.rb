dfbb32f7e5f95288e3d3487d9dfd57efb5bad773
Gonzalo Vallejos <hello@gonzalovallejos.com>
2026-07-08 15:52:27 -0700

    Update local-slack checksums for rebuilt v0.1.0 binaries
    The v0.1.0 release binaries were rebuilt (composer auto-grow fix in
    the web UI) and re-uploaded in place; checksums need to match.

--- Formula/local-slack.rb
+++ Formula/local-slack.rb
@@ -7,22 +7,22 @@ class LocalSlack < Formula
   on_macos do
     on_arm do
       url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-darwin-arm64.tar.gz"
-      sha256 "b7410e334424b10492683ce149be42f2e951cf7e331458ec1429a45f431131f9"
+      sha256 "9ae1bd6bd406d3140b9ac5f75599caacc6434fa1affa8271b321fe5e24f0ed5c"
     end
     on_intel do
       url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-darwin-x64.tar.gz"
-      sha256 "74bb7573b1a542888f38d4d86d126a8ade254f3831c79b9fae4a22f2599cec89"
+      sha256 "6ff48bb1fee08c0b9789e517c091808e390e84e405b3311af08b202e1c122ab6"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-linux-arm64.tar.gz"
-      sha256 "784b68e106845d72f73dad9bf254d662c0149a6e951a01bbae874a6120f94fc0"
+      sha256 "12b3d49b8a629a46fffbceb66276000fb03947668d05debe99e52e7822ee04f4"
     end
     on_intel do
       url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-linux-x64.tar.gz"
-      sha256 "b462325b2a7c929527a731052fd724ab56a3adecc145c588771f2a815ed2abe6"
+      sha256 "c3760c2f8af217a3500705e096f69b265eb8e9052b16f9832ce5e586738ba2c1"
     end
   end
 
