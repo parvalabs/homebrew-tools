b4267e2a825f55763ace2ea5c0385e031b20841e
Gonzalo Vallejos <hello@gonzalovallejos.com>
2026-07-09 22:51:20 -0700

    Update local-slack checksums for rebuilt v0.1.0 binaries
    Adds GET support on the Web API surface, fixing clients like Python's
    slack_sdk that call read-only methods with GET instead of POST.

--- Formula/local-slack.rb
+++ Formula/local-slack.rb
@@ -7,22 +7,22 @@ class LocalSlack < Formula
   on_macos do
     on_arm do
       url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-darwin-arm64.tar.gz"
-      sha256 "8e152e12f8ce772bf22217654d42ab01c376c59d27983c27a82cb3c1bf1dcb6d"
+      sha256 "b30cab013d4bd29b650469b33af00ba2ae0919f7c195cd19b183ade969da4d9a"
     end
     on_intel do
       url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-darwin-x64.tar.gz"
-      sha256 "25b7ea37b0b3360ad23ba91f1bfbea9b57408538f1f3b9c75f8034a0237d8676"
+      sha256 "5e80b97667c21c9a68a32c3ff467305195a874021088ba582a93e6cb6dfdfd49"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-linux-arm64.tar.gz"
-      sha256 "09b6b82926a2e8e5491cedd3af7e27e91993f9f4bc9303a991a175969c480509"
+      sha256 "6bdd3ec6cfead6027de14d65c2156f5818c2b75c95af9d3f7d253a2f3a588d82"
     end
     on_intel do
       url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-linux-x64.tar.gz"
-      sha256 "95a6e42010edfe3d01a9760ae20515ea0f9db3ade417d4b0180cd43a6c8cd10f"
+      sha256 "6a7ec78b1ba2a6e026a0d0ab6efd9f647ab1cb333e192da82a38f334a6bb57e5"
     end
   end
 
