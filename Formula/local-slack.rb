a5bc8c9ba5c886df41ddc6eda43c73e8d5e049a5
Gonzalo Vallejos <hello@gonzalovallejos.com>
2026-07-13 23:07:48 -0700

    Update local-slack to 0.1.1

--- Formula/local-slack.rb
+++ Formula/local-slack.rb
@@ -1,28 +1,28 @@
 class LocalSlack < Formula
   desc "Local, throwaway Slack for developing and testing Slack apps/bots"
   homepage "https://github.com/parvalabs/local-slack"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-darwin-arm64.tar.gz"
-      sha256 "b86935e7432e4f13b7badbcd6a02ee0e448b5342d57ecec494adb032019f7bb5"
+      url "https://github.com/parvalabs/local-slack/releases/download/v0.1.1/local-slack-darwin-arm64.tar.gz"
+      sha256 "65cdfc115e5bf44f01e8bd15c596558ba9163224fdca65ea9f778301854d7698"
     end
     on_intel do
-      url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-darwin-x64.tar.gz"
-      sha256 "22c16c5ffdbb3661aa69cbca800fada8837087228b2d0b84308541b25dac3905"
+      url "https://github.com/parvalabs/local-slack/releases/download/v0.1.1/local-slack-darwin-x64.tar.gz"
+      sha256 "e41aa9799713780d0e34122675e045b5de1a0b2b207728eca313d1dff89ff7d2"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-linux-arm64.tar.gz"
-      sha256 "4e547c840f0e885ef36604ab21da90e27bf9995440f719e32f9e07582790d495"
+      url "https://github.com/parvalabs/local-slack/releases/download/v0.1.1/local-slack-linux-arm64.tar.gz"
+      sha256 "08fbb94f87657fea7a66db9ada9e9f3e49acef56704a260fb348b2a0c66f36b0"
     end
     on_intel do
-      url "https://github.com/parvalabs/local-slack/releases/download/v0.1.0/local-slack-linux-x64.tar.gz"
-      sha256 "47c5a3003836896a2b3981bf4921bd08687332282f1fc3ac3ac1c3cdc44ee67a"
+      url "https://github.com/parvalabs/local-slack/releases/download/v0.1.1/local-slack-linux-x64.tar.gz"
+      sha256 "55e58ced40e60e489303bffed17f7020d40d55e113b2b77b3c9a1684af1e18e5"
     end
   end
 
